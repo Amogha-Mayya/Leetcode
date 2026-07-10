33427d7efa989444b43d501a691b3e8b9016794d
Amogha Mayya <68181503+Amogha-Mayya@users.noreply.github.com>
2026-07-10 13:13:02 +0530

    Time: 3 ms (27.87%) | Memory: 9.9 MB (60.29%) - LeetSync

--- 78-subsets/subsets.cpp
+++ 78-subsets/subsets.cpp
@@ -2,16 +2,13 @@ class Solution {
 public:
 void solve(int idx,vector<int>& nums,vector<vector<int>>& v,
 vector<int>& temp){
-    if(idx == nums.size())
-    {
+    if(idx == nums.size()){
         v.push_back(temp);
         return;
     }
-    // pick
     temp.push_back(nums[idx]);
     solve(idx+1,nums,v,temp);
     temp.pop_back();
-    // not pick
     solve(idx+1,nums,v,temp);
 }
     vector<vector<int>> subsets(vector<int>& nums) {
