20d85c61619038581ec3a02e92ad1c707d378b16
Amogha Mayya <68181503+Amogha-Mayya@users.noreply.github.com>
2026-07-11 19:27:57 +0530

    Time: 47 ms (54.05%) | Memory: 57.4 MB (58.36%) - LeetSync

--- 131-palindrome-partitioning/palindrome-partitioning.cpp
+++ 131-palindrome-partitioning/palindrome-partitioning.cpp
@@ -1,33 +1,34 @@
 class Solution {
 public:
-bool check_palindrome(string& str){
-    int i=0;
-    int j = str.size()-1;
-    while(i<j){
-        if(str[i] != str[j]) return false;
-        i++;j--;
+bool check_palindrome(string str){
+    int l = 0;
+    int r = str.size()-1;
+    while(l<r){
+        if(str[l] != str[r]) return false;
+        l++;r--;
     }
     return true;
 }
 void solve(int idx,string s,vector<vector<string>>& v,
-vector<string>& temp){
-    if(idx == s.size()){
+vector<string>& temp,int n){
+    if(idx == n){
         v.push_back(temp);
         return;
     }
-    for(int j=idx;j<s.size();j++){
+    for(int j=idx;j<n;j++){
         string str = s.substr(idx,j-idx+1);
         if(check_palindrome(str)){
             temp.push_back(str);
-            solve(j+1,s,v,temp);
+            solve(j+1,s,v,temp,n);
             temp.pop_back();
         }
     }
 }
     vector<vector<string>> partition(string s) {
+        int n = s.size();
         vector<vector<string>>v;
         vector<string>temp;
-        solve(0,s,v,temp);
+        solve(0,s,v,temp,n);
         return v;
     }
 };
