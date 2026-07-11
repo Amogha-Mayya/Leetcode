9f3a0fc65429e5421d21b65bbb3162ba0f0e7648
Amogha Mayya <68181503+Amogha-Mayya@users.noreply.github.com>
2026-07-11 11:35:37 +0530

    Time: 394 ms (39.93%) | Memory: 11.8 MB (19.08%) - LeetSync

--- 79-word-search/word-search.cpp
+++ 79-word-search/word-search.cpp
@@ -1,19 +1,21 @@
 class Solution {
 public:
-vector<vector<int>>c = {
+vector<vector<int>> c = {
     {1,0},{0,1},{-1,0},{0,-1}
 };
-bool solve(int x,int y,vector<vector<char>>& board, string word,int idx,int m,int n,vector<vector<int>>& vis){
+bool solve(int idx,int x,int y,vector<vector<char>>& board,
+vector<vector<int>>& vis,string word,int m,int n){
+    // base case here
     if(idx == word.size()){
         return true;
     }
     for(int k=0;k<c.size();k++){
         int xn = x + c[k][0];
         int yn = y + c[k][1];
-        if(xn >=0 && yn >=0 && xn < m && yn < n && !vis[xn][yn]
-        && (board[xn][yn] == word[idx])){
+        if(xn >= 0 && yn >=0 && xn < m && yn < n
+        && !vis[xn][yn] && board[xn][yn] == word[idx]){
             vis[xn][yn] = 1;
-            if(solve(xn,yn,board,word,idx+1,m,n,vis)) return true;
+            if(solve(idx+1,xn,yn,board,vis,word,m,n)) return true;
             vis[xn][yn] = 0;
         }
     }
@@ -27,7 +29,7 @@ bool solve(int x,int y,vector<vector<char>>& board, string word,int idx,int m,in
             for(int j=0;j<n;j++){
                 if(board[i][j] == word[0]){
                     vis[i][j] = 1;
-                    if(solve(i,j,board,word,1,m,n,vis)) return true;
+                    if(solve(1,i,j,board,vis,word,m,n)) return true;
                     vis[i][j] = 0;
                 }
             }
