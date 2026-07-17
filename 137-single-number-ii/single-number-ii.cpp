080660fd7f688a6f8cd14c6980ede74126583736
Amogha Mayya <68181503+Amogha-Mayya@users.noreply.github.com>
2026-07-17 18:55:03 +0530

    Time: 3 ms (31.39%) | Memory: 13.2 MB (99.41%) - LeetSync

--- 137-single-number-ii/single-number-ii.cpp
+++ 137-single-number-ii/single-number-ii.cpp
@@ -1,14 +1,14 @@
 class Solution {
 public:
     int singleNumber(vector<int>& nums) {
+        int n = nums.size();
         int temp = 0;
         for(int bit=0;bit<=31;bit++){
             int count = 0;
             for(auto i:nums){
-                if(i&(1<<bit))
-                    count++;
+                if(i&(1<<bit)) count++;
             }
-            if(count%3 !=0)
+            if(count%3 != 0)
                 temp = temp|(1<<bit);
         }
         return temp;
