aea1648323fa78fd9b025151f4bcdd1b778e94d6
Amogha Mayya <68181503+Amogha-Mayya@users.noreply.github.com>
2026-07-17 16:09:39 +0530

    Time: 1 ms (44.08%) | Memory: 13.4 MB (64.16%) - LeetSync

---
+++ 137-single-number-ii/single-number-ii.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        int temp = 0;
+        for(int bit=0;bit<=31;bit++){
+            int count = 0;
+            for(auto i:nums){
+                if(i&(1<<bit))
+                    count++;
+            }
+            if(count%3 !=0)
+                temp = temp|(1<<bit);
+        }
+        return temp;
+    }
+};
