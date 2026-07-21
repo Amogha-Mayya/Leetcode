76c0f0be63d8fd3637628ddf699a4699f21c3453
Amogha Mayya <68181503+Amogha-Mayya@users.noreply.github.com>
2026-07-21 22:18:21 +0530

    Time: 5 ms (51.50%) | Memory: 23.9 MB (30.57%) - LeetSync

--- 56-merge-intervals/merge-intervals.cpp
+++ 56-merge-intervals/merge-intervals.cpp
@@ -6,9 +6,9 @@ public:
         v.push_back(intervals[0]);
         int n = intervals.size();
         for(int i=1;i<n;i++){
-            vector<int>& prev = v.back();
-            if(intervals[i][0] <= prev[1]){
-                prev[1] = max(prev[1],intervals[i][1]);
+            vector<int>& temp = v.back();
+            if(intervals[i][0] <= temp[1]){
+                temp[1] = max(temp[1],intervals[i][1]);
             }
             else v.push_back(intervals[i]);
         }
