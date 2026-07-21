e355f603277781b00cebde58396c5eddc51f76e6
Amogha Mayya <68181503+Amogha-Mayya@users.noreply.github.com>
2026-07-21 22:32:48 +0530

    Time: 75 ms (5.46%) | Memory: 16.1 MB (8.91%) - LeetSync

--- 1222-remove-covered-intervals/remove-covered-intervals.cpp
+++ 1222-remove-covered-intervals/remove-covered-intervals.cpp
@@ -2,27 +2,27 @@ class Solution {
 public:
     int removeCoveredIntervals(vector<vector<int>>& intervals) {
         int n = intervals.size();
-        set<int>s;
-        // forward
-        for(int i=0;i<n-1;i++){
+        unordered_set<int>s;
+        for(int i=0;i<n;i++){
             int a = intervals[i][0];
             int b = intervals[i][1];
             for(int j=i+1;j<n;j++){
                 int c = intervals[j][0];
                 int d = intervals[j][1];
-                if((c <= a) && (b <= d))
+                if(c <= a && b <= d){
                     s.insert(i);
+                }
             }
         }
-        // backward
-        for(int i=0;i<n-1;i++){
-            int a = intervals[i][0];
-            int b = intervals[i][1];
+        for(int i=0;i<n;i++){
+            int c = intervals[i][0];
+            int d = intervals[i][1];
             for(int j=i+1;j<n;j++){
-                int c = intervals[j][0];
-                int d = intervals[j][1];
-                if((c >= a) && (b >= d))
+                int a = intervals[j][0];
+                int b = intervals[j][1];
+                if(c <= a && b <= d){
                     s.insert(j);
+                }
             }
         }
         return n - s.size();
