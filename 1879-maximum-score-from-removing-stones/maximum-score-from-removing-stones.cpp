03b432a92de98470524e1361f0090a654241c809
Amogha Mayya <68181503+Amogha-Mayya@users.noreply.github.com>
2026-07-23 21:56:49 +0530

    Time: 57 ms (26.95%) | Memory: 8.1 MB (25.93%) - LeetSync

--- 1879-maximum-score-from-removing-stones/maximum-score-from-removing-stones.cpp
+++ 1879-maximum-score-from-removing-stones/maximum-score-from-removing-stones.cpp
@@ -2,11 +2,11 @@ class Solution {
 public:
     int maximumScore(int a, int b, int c) {
         priority_queue<int>q;
-        q.push(a);q.push(b);q.push(c);
+        q.push(a); q.push(b); q.push(c);
         int count = 0;
-        while(q.size() > 1){
-            int x = q.top();q.pop();
-            int y = q.top();q.pop();
+        while(q.size() >= 2){
+            int x = q.top(); q.pop();
+            int y = q.top(); q.pop();
             if(x-1 > 0) q.push(--x);
             if(y-1 > 0) q.push(--y);
             count++;
