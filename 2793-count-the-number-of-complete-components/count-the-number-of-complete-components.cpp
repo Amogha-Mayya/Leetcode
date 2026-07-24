52bcb4cacad116e8831a552444186121b37f4d0d
Amogha Mayya <68181503+Amogha-Mayya@users.noreply.github.com>
2026-07-24 20:18:46 +0530

    Time: 84 ms (15.26%) | Memory: 141.9 MB (14.77%) - LeetSync

--- 2793-count-the-number-of-complete-components/count-the-number-of-complete-components.cpp
+++ 2793-count-the-number-of-complete-components/count-the-number-of-complete-components.cpp
@@ -1,24 +1,17 @@
 class Solution {
 public:
-void dfs(int node,unordered_map<int,vector<int>>& m,vector<int>& vis,
+void dfs(int node,vector<int>& vis,unordered_map<int,vector<int>>& m,
 vector<int>& temp){
     vis[node] = 1;
     temp.push_back(node);
-    for(auto x:m[node]){
-        if(!vis[x]){
-            dfs(x,m,vis,temp);
-        }
+    for(auto i:m[node]){
+        if(!vis[i])
+            dfs(i,vis,m,temp);
     }
-}
-bool check(unordered_map<int,vector<int>>& m,vector<int>& temp){
-    int num_nodes = temp.size();
-    for(auto i:temp){
-        if(m[i].size() == num_nodes-1) continue;
-        else return false;
-    }
-    return true;
+
 }
     int countCompleteComponents(int n, vector<vector<int>>& edges) {
+        int count = 0;
         unordered_map<int,vector<int>>m;
         for(auto i:edges){
             int a = i[0];
@@ -27,15 +20,21 @@ bool check(unordered_map<int,vector<int>>& m,vector<int>& temp){
             m[b].push_back(a);
         }
         vector<int>vis(n,0);
-        int count = 0;
+        int ans = 0;
         for(int i=0;i<n;i++){
+            vector<int>temp;
             if(!vis[i]){
-                vector<int>temp;
-                dfs(i,m,vis,temp);
-                if(check(m,temp))
-                    count++;
+                dfs(i,vis,m,temp);
             }
+            else continue;
+            int num = temp.size();
+            bool flag = true;
+            for(auto i:temp){
+                if(m[i].size() != (num-1))
+                    flag = false;
+            }
+            if(flag == true) ans++;
         }
-        return count;
+        return ans;
     }
 };
