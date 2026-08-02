88b80c65583d9c999ec2e92eba36aab3a2ef26a0
Amogha Mayya <68181503+Amogha-Mayya@users.noreply.github.com>
2026-08-02 20:16:06 +0530

    Time: 0 ms (100.00%) | Memory: 9.5 MB (50.00%) - LeetSync

---
+++ 4365-count-valid-prefixes/count-valid-prefixes.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int countValidPrefixes(string s) {
+        int one = 0;
+        int zero = 0;
+        int count = 0;
+        for(char ch : s){
+            if(ch == '0') zero++;
+            else one++;
+            if(abs(zero - one) <= 1) count++;
+        }
+        return count;
+    }
+};
