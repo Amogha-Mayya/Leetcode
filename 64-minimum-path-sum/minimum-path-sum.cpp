a5b20bdd958dea210c38f08bb93c9f67889792a4
Amogha Mayya <68181503+Amogha-Mayya@users.noreply.github.com>
2026-08-08 12:28:26 +0530

    Time: 3 ms (30.25%) | Memory: 16.4 MB (42.98%) - LeetSync

--- 64-minimum-path-sum/minimum-path-sum.cpp
+++ 64-minimum-path-sum/minimum-path-sum.cpp
@@ -1,27 +1,28 @@
 class Solution {
 public:
 vector<vector<int>>c = {
-    {0,1},{1,0}
+    {1,0},{0,1}
 };
-int solve(int x,int y,int m,int n,vector<vector<int>>& dp,
-vector<vector<int>>& grid){
-    if(x == m-1 && y == n-1) return grid[x][y];
+int solve(int x,int y,int m,int n,vector<vector<int>>& grid,
+vector<vector<int>>& dp){
+    if(x == m-1 && y == n-1){
+        return grid[x][y];
+    }
     if(dp[x][y] != -1) return dp[x][y];
-
-    int down = INT_MAX;
-    if(x+1 >=0 && x+1 < m){
-        down = grid[x][y] + solve(x+1,y,m,n,dp,grid);
+    int ans = INT_MAX;
+    for(int k=0;k<c.size();k++){
+        int xn = x + c[k][0];
+        int yn = y + c[k][1];
+        if(xn >=0 && yn >=0 && xn < m && yn < n){
+            ans = min(ans,grid[x][y] + solve(xn,yn,m,n,grid,dp));
+        }
     }
-    int right = INT_MAX;
-    if(y+1 >=0 && y+1 < n){
-        right = grid[x][y] + solve(x,y+1,m,n,dp,grid);
-    }
-    return dp[x][y] = min(down,right);
+    return dp[x][y] = ans;
 }
     int minPathSum(vector<vector<int>>& grid) {
         int m = grid.size();
         int n = grid[0].size();
-        vector<vector<int>>dp(m+1,vector<int>(n+1,-1));
-        return solve(0,0,m,n,dp,grid);
+        vector<vector<int>>dp(m,vector<int>(n,-1));
+        return solve(0,0,m,n,grid,dp);
     }
 };
