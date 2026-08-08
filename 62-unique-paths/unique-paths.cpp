36627dbdc729b5b85c44dcb091eb31846a3eb5a8
Amogha Mayya <68181503+Amogha-Mayya@users.noreply.github.com>
2026-08-08 12:13:51 +0530

    Time: 3 ms (9.55%) | Memory: 9.3 MB (55.10%) - LeetSync

--- 62-unique-paths/unique-paths.cpp
+++ 62-unique-paths/unique-paths.cpp
@@ -3,19 +3,20 @@ public:
 vector<vector<int>>c = {
     {0,1},{1,0}
 };
-int solve(int x,int y,int m,int n,
-vector<vector<int>>& dp){
-    if(x == m-1 && y == n-1) return 1;
-    if(dp[x][y] != -1) return dp[x][y];
+int solve(int row,int col,int m,int n,vector<vector<int>>& dp){
+    if(row == m-1 && col == n-1){
+        return 1;
+    }
+    if(dp[row][col] != -1) return dp[row][col];
     int ans = 0;
     for(int k=0;k<c.size();k++){
-        int xn = x + c[k][0];
-        int yn = y + c[k][1];
+        int xn = row + c[k][0];
+        int yn = col + c[k][1];
         if(xn >=0 && yn >=0 && xn < m && yn < n){
             ans += solve(xn,yn,m,n,dp);
         }
     }
-    return dp[x][y] = ans;
+    return dp[row][col] = ans;
 }
     int uniquePaths(int m, int n) {
         vector<vector<int>>dp(m+1,vector<int>(n+1,-1));
