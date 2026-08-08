0dc2e10f719a3d4e818e505ee574d50c3bbfd206
Amogha Mayya <68181503+Amogha-Mayya@users.noreply.github.com>
2026-08-08 12:48:29 +0530

    Time: 430 ms (9.47%) | Memory: 21.4 MB (64.43%) - LeetSync

--- 1224-minimum-falling-path-sum-ii/minimum-falling-path-sum-ii.cpp
+++ 1224-minimum-falling-path-sum-ii/minimum-falling-path-sum-ii.cpp
@@ -1,24 +1,26 @@
 class Solution {
 public:
-int solve(vector<vector<int>>& matrix,int n,int x,int y,
+int solve(int x,int y,vector<vector<int>>& matrix,int n,
 vector<vector<int>>& dp){
     if(x == n-1) return matrix[x][y];
-    if(dp[x][y] != -999) return dp[x][y];
+    if(dp[x][y] != INT_MIN) return dp[x][y];
     int ans = INT_MAX;
-    for(int col = 0; col < n; col++){
-        if(col == y) continue;
-        ans = min(ans,
-                matrix[x][y] + solve(matrix, n, x + 1, col, dp));
+    for(int k=-n+1;k<n;k++){
+        int xn = x + 1;
+        int yn = y + k;
+        if(xn >=0 && yn >=0 && xn < n && yn < n && y!=yn){
+            ans = min(ans,matrix[x][y] + solve(xn,yn,matrix,n,dp));
+        }
     }
     return dp[x][y] = ans;
 }
     int minFallingPathSum(vector<vector<int>>& grid) {
         int n = grid.size();
-        vector<vector<int>>dp(n+1,vector<int>(n+1,-999));
-        int mini = INT_MAX;
+        vector<vector<int>>dp(n,vector<int>(n,INT_MIN));
+        int ans = INT_MAX;
         for(int i=0;i<n;i++){
-            mini = min(mini,solve(grid,n,0,i,dp));
+            ans = min(ans,solve(0,i,grid,n,dp));
         }
-        return mini;
+        return ans;
     }
 };
