6bb672d81f68f0e6bf3a02c836366e747919feb6
Amogha Mayya <68181503+Amogha-Mayya@users.noreply.github.com>
2026-08-08 12:43:48 +0530

    Time: 3 ms (39.10%) | Memory: 15.2 MB (26.72%) - LeetSync

--- 967-minimum-falling-path-sum/minimum-falling-path-sum.cpp
+++ 967-minimum-falling-path-sum/minimum-falling-path-sum.cpp
@@ -1,31 +1,29 @@
 class Solution {
 public:
-vector<vector<int>>c={
-    {1,-1},{1,0},{1,1}
+vector<vector<int>>c = {
+    {1,0},{1,1},{1,-1}
 };
-int solve(vector<vector<int>>& matrix,int n,int x,int y,
+int solve(int x,int y,vector<vector<int>>& matrix,int n,
 vector<vector<int>>& dp){
     if(x == n-1) return matrix[x][y];
-    if(dp[x][y] != -999) return dp[x][y];
+    if(dp[x][y] != INT_MIN) return dp[x][y];
     int ans = INT_MAX;
     for(int k=0;k<c.size();k++){
-        int x_n = x + c[k][0];
-        int y_n = y + c[k][1];
-        if(x_n >=0 && y_n >=0 && x_n < n && y_n < n)
-        {
-            ans = min(ans,matrix[x][y] + solve(matrix,n,x_n,y_n,dp));
+        int xn = x + c[k][0];
+        int yn = y + c[k][1];
+        if(xn >=0 && yn >=0 && xn < n && yn < n){
+            ans = min(ans,matrix[x][y] + solve(xn,yn,matrix,n,dp));
         }
     }
     return dp[x][y] = ans;
 }
     int minFallingPathSum(vector<vector<int>>& matrix) {
         int n = matrix.size();
-        if(n==100 && matrix[0][0]==0 && matrix[0][1] == 1) return 99;
-        vector<vector<int>>dp(n+1,vector<int>(n+1,-999));
-        int mini = INT_MAX;
+        vector<vector<int>>dp(n,vector<int>(n,INT_MIN));
+        int ans = INT_MAX;
         for(int i=0;i<n;i++){
-            mini = min(mini,solve(matrix,n,0,i,dp));
+            ans = min(ans,solve(0,i,matrix,n,dp));
         }
-        return mini;
+        return ans;
     }
 };
