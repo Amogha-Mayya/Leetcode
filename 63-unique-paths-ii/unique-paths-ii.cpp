466af55498f36ae9eb9a979bcaf83812a964101f
Amogha Mayya <68181503+Amogha-Mayya@users.noreply.github.com>
2026-08-08 12:16:23 +0530

    Time: 1 ms (15.57%) | Memory: 12 MB (6.61%) - LeetSync

--- 63-unique-paths-ii/unique-paths-ii.cpp
+++ 63-unique-paths-ii/unique-paths-ii.cpp
@@ -3,27 +3,27 @@ public:
 vector<vector<int>>c = {
     {0,1},{1,0}
 };
-int solve(int x,int y,int m,int n,vector<vector<int>>& obstacleGrid,
-vector<vector<int>>& dp){
-    if(x == m-1 && y == n-1){
+int solve(int row,int col,int m,int n,vector<vector<int>>& dp,
+vector<vector<int>>& obstacleGrid){
+    if(row == m-1 && col == n-1){
         return 1;
     }
-    if(dp[x][y] != -1) return dp[x][y];
+    if(dp[row][col] != -1) return dp[row][col];
     int ans = 0;
     for(int k=0;k<c.size();k++){
-        int xn = x + c[k][0];
-        int yn = y + c[k][1];
-        if(xn >=0 && yn >=0 && xn < m && yn < n && obstacleGrid[xn][yn] != 1){
-            ans += solve(xn,yn,m,n,obstacleGrid,dp);
+        int xn = row + c[k][0];
+        int yn = col + c[k][1];
+        if(xn >=0 && yn >=0 && xn < m && yn < n && obstacleGrid[xn][yn] == 0){
+            ans += solve(xn,yn,m,n,dp,obstacleGrid);
         }
     }
-    return dp[x][y] = ans;
+    return dp[row][col] = ans;
 }
     int uniquePathsWithObstacles(vector<vector<int>>& obstacleGrid) {
         int m = obstacleGrid.size();
         int n = obstacleGrid[0].size();
-        if(obstacleGrid[m-1][n-1] == 1 || obstacleGrid[0][0] == 1) return 0;
         vector<vector<int>>dp(m+1,vector<int>(n+1,-1));
-        return solve(0,0,m,n,obstacleGrid,dp);
+        if(obstacleGrid[0][0] == 1 || obstacleGrid[m-1][n-1] == 1) return -0;
+        return solve(0,0,m,n,dp,obstacleGrid);
     }
 };
