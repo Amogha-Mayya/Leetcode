b31aa30f9c2446a0240ec1c50857de180aab0730
Amogha Mayya <68181503+Amogha-Mayya@users.noreply.github.com>
2026-08-08 10:03:14 +0530

    Time: 0 ms (100.00%) | Memory: 8.5 MB (39.03%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.cpp
+++ 70-climbing-stairs/climbing-stairs.cpp
@@ -1,16 +1,19 @@
 class Solution {
 public:
+int solve(int idx,int n,vector<int>& dp){
+    if(idx == n){
+        return 1;
+    }
+    if(dp[idx] != -1) return dp[idx];
+    int pick1 = 0,pick2 = 0;
+    if(idx + 1 <= n)
+    pick1 = solve(idx+1,n,dp);
+    if(idx + 2 <= n)
+    pick2 = solve(idx+2,n,dp);
+    return dp[idx] = (pick1 + pick2);
+}
     int climbStairs(int n) {
-        vector<int>dp(n+1,0);
-        dp[0] = 1;
-        for(int i=1;i<=n;i++){
-            int pick1 = 0, pick2 = 0;
-            if(i - 1 >=0)
-                pick1 = dp[i-1];
-            if(i - 2 >=0)
-                pick2 = dp[i-2];
-            dp[i] = pick1 + pick2;
-        }
-        return dp[n];
+        vector<int>dp(n,-1);
+        return solve(0,n,dp);
     }
 };
