39a0784d8b3ba2da739f38d72ebece7c00843a71
Amogha Mayya <68181503+Amogha-Mayya@users.noreply.github.com>
2026-08-08 12:35:20 +0530

    Time: 0 ms (100.00%) | Memory: 13.5 MB (9.17%) - LeetSync

--- 120-triangle/triangle.cpp
+++ 120-triangle/triangle.cpp
@@ -1,17 +1,19 @@
 class Solution {
 public:
-int solve(int x,int y,vector<vector<int>>& triangle,int m,int n,
+int solve(int row,int col,vector<vector<int>>& triangle,
 vector<vector<int>>& dp){
-    if(x == m-1) return triangle[x][y];
-    int pick1 = INT_MAX , pick2 = INT_MAX;
-    if(dp[x][y] != INT_MIN) return dp[x][y];
-    pick1 = triangle[x][y] + solve(x+1,y,triangle,m,n,dp);
-    pick2 = triangle[x][y] + solve(x+1,y+1,triangle,m,n,dp);
-    return dp[x][y] = min(pick1,pick2);
+    if(row == triangle.size()-1){
+        return triangle[row][col];
+    }
+    if(dp[row][col] != INT_MIN) return dp[row][col];
+    int pick1 = INT_MAX,pick2 = INT_MAX;
+    pick1 = triangle[row][col] + solve(row+1,col,triangle,dp);
+    pick2 = triangle[row][col] + solve(row+1,col+1,triangle,dp);
+    return dp[row][col] = min(pick1,pick2);
 }
     int minimumTotal(vector<vector<int>>& triangle) {
-        int m = triangle.size();
-        vector<vector<int>>dp(m+1,vector<int>(m+1,INT_MIN));
-        return solve(0,0,triangle,m,m,dp);
+        int n = triangle.size();
+        vector<vector<int>>dp(n,vector<int>(n,INT_MIN));
+        return solve(0,0,triangle,dp);
     }
 };
