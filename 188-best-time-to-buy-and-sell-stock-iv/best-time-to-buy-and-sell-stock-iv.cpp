ecb63f6f4ce4ecd0f843c29e9f51aa2140d2d147
Amogha Mayya <68181503+Amogha-Mayya@users.noreply.github.com>
2026-08-13 22:49:26 +0530

    Time: 20 ms (12.12%) | Memory: 18.3 MB (41.97%) - LeetSync

--- 188-best-time-to-buy-and-sell-stock-iv/best-time-to-buy-and-sell-stock-iv.cpp
+++ 188-best-time-to-buy-and-sell-stock-iv/best-time-to-buy-and-sell-stock-iv.cpp
@@ -1,23 +1,21 @@
 class Solution {
 public:
-int solve(int idx,vector<int>& prices,bool have_share,int trans,
-vector<vector<vector<int>>>& dp){
-    if(idx == prices.size() || trans == 0) return 0;
-    int buy = 0,sell = 0, hold = 0, wait = 0;
-    if(dp[idx][have_share][trans] != -1) return dp[idx][have_share][trans];
-    if(have_share == false){
-    buy = -1 * prices[idx] + solve(idx+1,prices,true,trans,dp);
-    wait = solve(idx+1,prices,false,trans,dp);
+int solve(int idx,vector<int>& prices,bool hold,
+vector<vector<vector<int>>>& dp,int k){
+    if(idx == prices.size() || k == 0) return 0;
+    int pick = 0,notpick = 0;
+    if(dp[idx][hold][k] != -1) return dp[idx][hold][k];
+    if(hold == false){
+        pick = max(-prices[idx] + solve(idx+1,prices,true,dp,k),solve(idx+1,prices,false,dp,k));
     }
-    if(have_share == true){
-    sell = prices[idx] + solve(idx+1,prices,false,trans-1,dp);
-    hold = solve(idx+1,prices,true,trans,dp);
+    else{
+        notpick = max(prices[idx] + solve(idx+1,prices,false,dp,k - 1),solve(idx+1,prices,true,dp,k));
     }
-    return dp[idx][have_share][trans] = max({buy,wait,sell,hold});
+    return dp[idx][hold][k] = max(pick,notpick);
 }
     int maxProfit(int k, vector<int>& prices) {
         int n = prices.size();
-vector<vector<vector<int>>>dp(n,vector<vector<int>>(2,vector<int>(k+1,-1)));
-        return solve(0,prices,false,k,dp);
+        vector<vector<vector<int>>>dp(n+1,vector<vector<int>>(2,vector<int>(k + 1,-1)));
+        return solve(0,prices,false,dp,k);
     }
 };
