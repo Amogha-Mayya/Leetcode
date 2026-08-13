08d9b10413b51a48c02fee2a412e8420a662131d
Amogha Mayya <68181503+Amogha-Mayya@users.noreply.github.com>
2026-08-13 22:12:09 +0530

    Time: 25 ms (58.74%) | Memory: 26.9 MB (43.52%) - LeetSync

--- 1170-shortest-common-supersequence/shortest-common-supersequence.cpp
+++ 1170-shortest-common-supersequence/shortest-common-supersequence.cpp
@@ -1,21 +1,18 @@
 class Solution {
 public:
     string shortestCommonSupersequence(string str1, string str2) {
-        int n = str1.size();
-        int m = str2.size();
-        vector<vector<int>>dp(n+1,vector<int>(m+1,0));
-        for(int i=1;i<=n;i++){
-            for(int j=1;j<=m;j++){
+        int n1 = str1.size();
+        int n2 = str2.size();
+        vector<vector<int>>dp(n1+1,vector<int>(n2+1,0));
+        for(int i=1;i<=n1;i++){
+            for(int j=1;j<=n2;j++){
                 if(str1[i-1] == str2[j-1]){
                     dp[i][j] = 1 + dp[i-1][j-1];
                 }
-                else{
-                    dp[i][j] = max(dp[i-1][j],dp[i][j-1]);
-                }
+                else dp[i][j] = max(dp[i-1][j],dp[i][j-1]);
             }
         }
-        int i = n;
-        int j = m;
+        int i = n1, j = n2;
         string ans = "";
         while(i > 0 && j > 0){
             if(str1[i-1] == str2[j-1]){
@@ -40,7 +37,7 @@ public:
             ans += str2[j-1];
             j--;
         }
-        reverse(ans.begin(), ans.end());
+        reverse(ans.begin(),ans.end());
         return ans;
     }
 };
