6b63a42bc55810a2333f8701790f9dfa96bbb134
Amogha Mayya <68181503+Amogha-Mayya@users.noreply.github.com>
2026-08-13 20:23:16 +0530

    Time: 39 ms (35.85%) | Memory: 61.8 MB (5.77%) - LeetSync

--- 518-coin-change-ii/coin-change-ii.cpp
+++ 518-coin-change-ii/coin-change-ii.cpp
@@ -3,16 +3,16 @@ public:
 typedef unsigned long long ll;
     int change(int amount, vector<int>& coins) {
         int n = coins.size();
-        vector<vector<int>>dp(n+1,vector<int>(amount+1,0));
+        vector<vector<ll>>dp(n+1,vector<ll>(amount+1,0));
         dp[0][0] = 1;
         for(int i=1;i<=n;i++){
             for(int j=0;j<=amount;j++){
-                ll take = 0;
+                ll pick = 0;
                 if(coins[i-1] <= j){
-                    take = dp[i][j-coins[i-1]];
+                    pick = dp[i][j - coins[i-1]];
                 }
-                ll nottake = dp[i-1][j];
-                dp[i][j] = (take + nottake);
+                ll notpick = dp[i-1][j];
+                dp[i][j] = (pick + notpick);
             }
         }
         return (int)dp[n][amount];
