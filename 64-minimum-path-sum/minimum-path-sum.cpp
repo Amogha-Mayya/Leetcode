f93c5d4836c99b5d86febfdf88b7981b803b3917
Amogha Mayya <68181503+Amogha-Mayya@users.noreply.github.com>
2026-08-14 23:40:22 +0530

    Time: 0 ms (100.00%) | Memory: 16.5 MB (16.72%) - LeetSync

--- 64-minimum-path-sum/minimum-path-sum.cpp
+++ 64-minimum-path-sum/minimum-path-sum.cpp
@@ -3,18 +3,16 @@ public:
 vector<vector<int>>c = {
     {1,0},{0,1}
 };
-int solve(int x,int y,int m,int n,vector<vector<int>>& grid,
+int solve(int x,int y,vector<vector<int>>& grid,int m,int n,
 vector<vector<int>>& dp){
-    if(x == m-1 && y == n-1){
-        return grid[x][y];
-    }
-    if(dp[x][y] != -1) return dp[x][y];
+    if(x == m-1 && y == n-1) return grid[x][y];
     int ans = INT_MAX;
+    if(dp[x][y] != -1) return dp[x][y];
     for(int k=0;k<c.size();k++){
         int xn = x + c[k][0];
         int yn = y + c[k][1];
         if(xn >=0 && yn >=0 && xn < m && yn < n){
-            ans = min(ans,grid[x][y] + solve(xn,yn,m,n,grid,dp));
+            ans = min(ans,grid[x][y] + solve(xn,yn,grid,m,n,dp));
         }
     }
     return dp[x][y] = ans;
@@ -23,6 +21,6 @@ vector<vector<int>>& dp){
         int m = grid.size();
         int n = grid[0].size();
         vector<vector<int>>dp(m,vector<int>(n,-1));
-        return solve(0,0,m,n,grid,dp);
+        return solve(0,0,grid,m,n,dp);
     }
 };
