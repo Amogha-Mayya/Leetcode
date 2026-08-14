a865c392791505045d250b187061bd4820d0ee48
Amogha Mayya <68181503+Amogha-Mayya@users.noreply.github.com>
2026-08-14 17:25:36 +0530

    Time: 36 ms (29.09%) | Memory: 177.9 MB (49.42%) - LeetSync

--- 4041-climbing-stairs-ii/climbing-stairs-ii.cpp
+++ 4041-climbing-stairs-ii/climbing-stairs-ii.cpp
@@ -4,12 +4,12 @@ public:
         costs.insert(costs.begin(),0);
         vector<int>dp(n+1,0);
         for(int i=1;i<=n;i++){
-            int pick1 = INT_MAX,pick2 = INT_MAX,pick3 = INT_MAX;
-            if(i-1 >= 0)
+            int pick1 = 1e9,pick2 = 1e9,pick3 = 1e9;
+            if(i - 1 >= 0)
             pick1 = costs[i] + 1 + dp[i-1];
-            if(i-2 >= 0)
+            if(i - 2 >= 0)
             pick2 = costs[i] + 4 + dp[i-2];
-            if(i-3 >= 0)
+            if(i - 3 >= 0)
             pick3 = costs[i] + 9 + dp[i-3];
             dp[i] = min({pick1,pick2,pick3});
         }
