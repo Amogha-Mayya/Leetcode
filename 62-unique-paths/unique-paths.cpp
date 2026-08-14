3610a4a8aa36416252bbb7500661359197088645
Amogha Mayya <68181503+Amogha-Mayya@users.noreply.github.com>
2026-08-14 22:30:15 +0530

    Time: 0 ms (100.00%) | Memory: 9.4 MB (55.30%) - LeetSync

--- 62-unique-paths/unique-paths.cpp
+++ 62-unique-paths/unique-paths.cpp
@@ -1,22 +1,20 @@
 class Solution {
 public:
 vector<vector<int>>c = {
-    {0,1},{1,0}
+    {1,0},{0,1}
 };
-int solve(int row,int col,int m,int n,vector<vector<int>>& dp){
-    if(row == m-1 && col == n-1){
-        return 1;
-    }
-    if(dp[row][col] != -1) return dp[row][col];
-    int ans = 0;
+int solve(int x,int y,int m,int n,vector<vector<int>>& dp){
+    if(x == m-1 && y == n-1) return 1;
+    int count = 0;
+    if(dp[x][y] != -1) return dp[x][y];
     for(int k=0;k<c.size();k++){
-        int xn = row + c[k][0];
-        int yn = col + c[k][1];
+        int xn = x + c[k][0];
+        int yn = y + c[k][1];
         if(xn >=0 && yn >=0 && xn < m && yn < n){
-            ans += solve(xn,yn,m,n,dp);
+            count += solve(xn,yn,m,n,dp); 
         }
     }
-    return dp[row][col] = ans;
+    return dp[x][y] = count;
 }
     int uniquePaths(int m, int n) {
         vector<vector<int>>dp(m+1,vector<int>(n+1,-1));
