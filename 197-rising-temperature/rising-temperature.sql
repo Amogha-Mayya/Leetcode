8967b56a48b23065700e6936d2beeb6522446ef3
Amogha Mayya <68181503+Amogha-Mayya@users.noreply.github.com>
2026-09-03 15:17:20 +0530

    Time: 281 ms (30.84%) | Memory: 0B (100.00%) - LeetSync

--- 197-rising-temperature/rising-temperature.sql
+++ 197-rising-temperature/rising-temperature.sql
@@ -3,5 +3,4 @@ select id from (select *,
 lag(recordDate) over (order by recordDate) as lag_date,
 lag(temperature) over (order by recordDate) as lag_temp
 from weather)
-where temperature > lag_temp and
-recorddate = lag_date + interval '1 days';
+where temperature > lag_temp and recorddate = interval '1 days' + lag_date;
