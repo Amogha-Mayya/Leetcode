ab63d8d4b429ddb3a3077e29a1ba35dbaeb6d338
Amogha Mayya <68181503+Amogha-Mayya@users.noreply.github.com>
2026-09-11 17:20:20 +0530

    Time: 88 ms (14.88%) | Memory: 142 MB (15.31%) - LeetSync

--- 2793-count-the-number-of-complete-components/count-the-number-of-complete-components.cpp
+++ 2793-count-the-number-of-complete-components/count-the-number-of-complete-components.cpp
@@ -1,18 +1,17 @@
 class Solution {
 public:
-void dfs(int node,vector<int>& vis,unordered_map<int,vector<int>>& m,
+void dfs(int node,unordered_map<int,vector<int>>& m,vector<int>& vis,
 vector<int>& temp){
     vis[node] = 1;
     temp.push_back(node);
     for(auto i:m[node]){
         if(!vis[i])
-            dfs(i,vis,m,temp);
+            dfs(i,m,vis,temp);
     }
-
 }
     int countCompleteComponents(int n, vector<vector<int>>& edges) {
-        int count = 0;
         unordered_map<int,vector<int>>m;
+        // adjacency list
         for(auto i:edges){
             int a = i[0];
             int b = i[1];
@@ -23,17 +22,17 @@ vector<int>& temp){
         int ans = 0;
         for(int i=0;i<n;i++){
             vector<int>temp;
-            if(!vis[i]){
-                dfs(i,vis,m,temp);
-            }
-            else continue;
-            int num = temp.size();
             bool flag = true;
-            for(auto i:temp){
-                if(m[i].size() != (num-1))
+            if(!vis[i]){
+                dfs(i,m,vis,temp);
+                for(auto i:temp){
+                if(m[i].size() != temp.size()-1){
                     flag = false;
+                    break;
+                    }
+                }
+                if(flag) ans++;
             }
-            if(flag == true) ans++;
         }
         return ans;
     }
