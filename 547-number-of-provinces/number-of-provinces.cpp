8d3b062b48352c3cbff9e5c1e8954d00d7a0a4d2
Amogha Mayya <68181503+Amogha-Mayya@users.noreply.github.com>
2026-09-11 17:00:34 +0530

    Time: 9 ms (5.82%) | Memory: 21.1 MB (12.89%) - LeetSync

--- 547-number-of-provinces/number-of-provinces.cpp
+++ 547-number-of-provinces/number-of-provinces.cpp
@@ -3,18 +3,20 @@ public:
 void dfs(int node,vector<int>& vis,unordered_map<int,vector<int>>& m){
     vis[node] = 1;
     for(auto i:m[node]){
-        if(!vis[i])
+        if(!vis[i]){
             dfs(i,vis,m);
+        }
     }
 }
     int findCircleNum(vector<vector<int>>& isConnected) {
-        // adjacency matrix
+        // adjacency matrix to adjacency list
         unordered_map<int,vector<int>>m;
         int n = isConnected.size();
         for(int i=0;i<n;i++){
             for(int j=0;j<n;j++){
                 if(i!=j && isConnected[i][j] == 1){
-                    m[i+1].push_back(j+1);
+                    m[i + 1].push_back(j + 1);
+                    m[j + 1].push_back(i + 1);
                 }
             }
         }
@@ -22,8 +24,8 @@ void dfs(int node,vector<int>& vis,unordered_map<int,vector<int>>& m){
         vector<int>vis(n+1,0);
         for(int i=1;i<=n;i++){
             if(!vis[i]){
-                count++;
                 dfs(i,vis,m);
+                count++;
             }
         }
         return count;
