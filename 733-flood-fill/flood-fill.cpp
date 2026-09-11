8e5365feb9d590ff82bf0ad960152294b7d9d296
Amogha Mayya <68181503+Amogha-Mayya@users.noreply.github.com>
2026-09-11 19:41:29 +0530

    Time: 0 ms (100.00%) | Memory: 19 MB (8.02%) - LeetSync

--- 733-flood-fill/flood-fill.cpp
+++ 733-flood-fill/flood-fill.cpp
@@ -6,11 +6,11 @@ vector<vector<int>>c = {
     vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int color) {
         int m = image.size();
         int n = image[0].size();
-        int clr = image[sr][sc];
+        int temp = image[sr][sc];
         queue<pair<int,int>>q;
+        vector<vector<int>>vis(m,vector<int>(n,0));
         q.push({sr,sc});
         image[sr][sc] = color;
-        vector<vector<int>>vis(m,vector<int>(n,0));
         vis[sr][sc] = 1;
         while(!q.empty()){
             auto [x,y] = q.front();
@@ -18,11 +18,11 @@ vector<vector<int>>c = {
             for(int k=0;k<c.size();k++){
                 int xn = x + c[k][0];
                 int yn = y + c[k][1];
-                if(xn >= 0 && yn >=0 && xn < m && yn < n && !vis[xn][yn]
-                && (image[xn][yn] == clr)){
+                if(xn>=0 && yn>=0 && xn<m && yn<n && !vis[xn][yn] && 
+                image[xn][yn] == temp){
+                    q.push({xn,yn});
                     vis[xn][yn] = 1;
                     image[xn][yn] = color;
-                    q.push({xn,yn});
                 }
             }
         }
