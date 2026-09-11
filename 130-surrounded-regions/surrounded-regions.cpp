3cf877cee253c352270243d3b9ed6d87ad242faa
Amogha Mayya <68181503+Amogha-Mayya@users.noreply.github.com>
2026-09-11 23:52:05 +0530

    Time: 0 ms (100.00%) | Memory: 14.9 MB (30.15%) - LeetSync

--- 130-surrounded-regions/surrounded-regions.cpp
+++ 130-surrounded-regions/surrounded-regions.cpp
@@ -1,21 +1,18 @@
 class Solution {
 public:
 vector<vector<int>>c = {
-    {1,0},{0,1},{-1,0},{0,-1}
+    {1,0},{0,1},{0,-1},{-1,0}
 };
     void solve(vector<vector<char>>& board) {
         int m = board.size();
         int n = board[0].size();
         queue<pair<int,int>>q;
-        vector<vector<int>>vis(m,vector<int>(n,0));
+        // push all the boundary O's
         for(int i=0;i<m;i++){
             for(int j=0;j<n;j++){
-                if(i == 0 || j == 0 || i == m-1 || j == n-1){
-                    if(!vis[i][j] && board[i][j] == 'O'){
-                        vis[i][j] = 1;
-                        board[i][j] = '#';
+                if(i==0||i==m-1||j==0||j==n-1){
+                    if(board[i][j] == 'O')
                         q.push({i,j});
-                    }
                 }
             }
         }
@@ -25,18 +22,23 @@ vector<vector<int>>c = {
             for(int k=0;k<c.size();k++){
                 int xn = x + c[k][0];
                 int yn = y + c[k][1];
-                if(xn > 0 && yn > 0 && xn < m-1 && yn < n-1
-                && !vis[xn][yn] && board[xn][yn] == 'O'){
-                    board[xn][yn] = '#';
-                    vis[xn][yn] = 1;
+                if(xn>=0 && yn>=0 && xn<m && yn<n
+                && board[xn][yn] == 'O'){
                     q.push({xn,yn});
+                    board[xn][yn] = '#';
                 }
+            }
+        }
+        for(int i=1;i<m-1;i++){
+            for(int j=1;j<n-1;j++){
+                if(board[i][j] == 'O')
+                    board[i][j] = 'X';
             }
         }
         for(int i=0;i<m;i++){
             for(int j=0;j<n;j++){
-                if(board[i][j] == 'O') board[i][j] = 'X';
-                if(board[i][j] == '#') board[i][j] = 'O';
+                if(board[i][j] == '#')
+                    board[i][j] = 'O';
             }
         }
     }
