29b506695cb7d2556521ddfe954fa6eae9895bfc
Amogha Mayya <68181503+Amogha-Mayya@users.noreply.github.com>
2026-09-12 10:18:44 +0530

    Time: 12 ms (80.61%) | Memory: 34.9 MB (64.94%) - LeetSync

--- 542-01-matrix/01-matrix.cpp
+++ 542-01-matrix/01-matrix.cpp
@@ -7,12 +7,13 @@ vector<vector<int>>c = {
         int m = mat.size();
         int n = mat[0].size();
         queue<pair<int,int>>q;
-        vector<vector<int>>ans(m,vector<int>(n,-1));
+        vector<vector<int>>grid(m,vector<int>(n,-1));
+        // push all 0's to queue
         for(int i=0;i<m;i++){
             for(int j=0;j<n;j++){
                 if(mat[i][j] == 0){
                     q.push({i,j});
-                    ans[i][j] = 0;
+                    grid[i][j] = 0;
                 }
             }
         }
@@ -22,13 +23,13 @@ vector<vector<int>>c = {
             for(int k=0;k<c.size();k++){
                 int xn = x + c[k][0];
                 int yn = y + c[k][1];
-                if(xn >=0 && yn >=0 && xn < m && yn < n
-                && ans[xn][yn] == -1){
-                    ans[xn][yn] = ans[x][y] + 1;
+                if(xn>=0 && yn>=0 && xn<m && yn<n
+                && grid[xn][yn] == -1){
+                    grid[xn][yn] = grid[x][y] + 1;
                     q.push({xn,yn});
                 }
             }
         }
-        return ans;
+        return grid;
     }
 };
