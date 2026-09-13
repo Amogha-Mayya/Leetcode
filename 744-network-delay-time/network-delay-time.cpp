47c5e7e1fee250f36fbe7f91f1ded33de23be50b
Amogha Mayya <68181503+Amogha-Mayya@users.noreply.github.com>
2026-09-13 22:31:53 +0530

    Time: 80 ms (67.39%) | Memory: 48 MB (18.62%) - LeetSync

--- 744-network-delay-time/network-delay-time.cpp
+++ 744-network-delay-time/network-delay-time.cpp
@@ -1,17 +1,18 @@
 class Solution {
 public:
-typedef pair<int,int>pp;
+typedef pair<int,int> pp;
     int networkDelayTime(vector<vector<int>>& times, int n, int k) {
-        unordered_map<int,vector<pair<int,int>>>m;
+        unordered_map<int,vector<pp>>m;
         for(auto i:times){
             int a = i[0];
             int b = i[1];
             int c = i[2];
             m[a].push_back({b,c});
         }
-        vector<int>dist(n + 1,1e9);
-        dist[k] = 0;
+        // (dist,node)
         priority_queue<pp,vector<pp>,greater<pp>>q;
+        vector<int>dist(n+1,1e9);
+        dist[k] = 0;
         q.push({0,k});
         while(!q.empty()){
             auto [d,u] = q.top();
@@ -20,16 +21,16 @@ typedef pair<int,int>pp;
             for(auto i:m[u]){
                 int v = i.first;
                 int wt = i.second;
-                if(wt + dist[u] < dist[v]){
+                // relax the edge
+                if(dist[u] + wt < dist[v]){
                     dist[v] = dist[u] + wt;
                     q.push({dist[v],v});
                 }
             }
         }
-        for(int i=1;i<dist.size();i++){
+        for(int i=1;i<=n;i++){
             if(dist[i] == 1e9) return -1;
         }
-        dist[0] = 0;
-        return *max_element(dist.begin(),dist.end());
+        return *max_element(dist.begin() + 1,dist.end());
     }
 };
