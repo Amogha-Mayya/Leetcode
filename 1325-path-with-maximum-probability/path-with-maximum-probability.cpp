ef5c483efe855f931fab95a42e753959066b1fe2
Amogha Mayya <68181503+Amogha-Mayya@users.noreply.github.com>
2026-09-13 23:59:45 +0530

    Time: 82 ms (8.74%) | Memory: 79.3 MB (5.09%) - LeetSync

--- 1325-path-with-maximum-probability/path-with-maximum-probability.cpp
+++ 1325-path-with-maximum-probability/path-with-maximum-probability.cpp
@@ -1,29 +1,28 @@
 class Solution {
 public:
-typedef pair<double,int>pp;
     double maxProbability(int n, vector<vector<int>>& edges, vector<double>& succProb, int start_node, int end_node) {
-        int x = 0;
         unordered_map<int,vector<pair<int,double>>>m;
+        int k = 0;
         for(auto i:edges){
             int a = i[0];
             int b = i[1];
-            double c = succProb[x++];
+            double c = succProb[k++];
             m[a].push_back({b,c});
             m[b].push_back({a,c});
         }
         vector<double>dist(n,0.0);
         dist[start_node] = 1.0;
-        priority_queue<pp>q;
+        priority_queue<pair<double,int>>q;
         q.push({1.0,start_node});
         while(!q.empty()){
             auto [d,u] = q.top();
             q.pop();
             for(auto i:m[u]){
-                double wt = i.second;
                 int v = i.first;
-                if(wt * dist[u] > dist[v]){
-                    q.push({dist[v],v});
+                double wt = i.second;
+                if(dist[u] * wt > dist[v]){
                     dist[v] = dist[u] * wt;
+                    q.push({dist[v],v});
                 }
             }
         }
