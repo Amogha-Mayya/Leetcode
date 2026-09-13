380bbb12ba25a5199f44bee7cbc40f0a0ba9253d
Amogha Mayya <68181503+Amogha-Mayya@users.noreply.github.com>
2026-09-13 16:47:48 +0530

    Time: 8 ms (35.27%) | Memory: 21.2 MB (6.06%) - LeetSync

--- 207-course-schedule/course-schedule.cpp
+++ 207-course-schedule/course-schedule.cpp
@@ -2,37 +2,28 @@ class Solution {
 public:
     bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
         int n = numCourses;
+        vector<int>indegree(n,0);
         unordered_map<int,vector<int>>m;
         for(auto i:prerequisites){
             int a = i[0];
             int b = i[1];
+            indegree[a]++;
             m[b].push_back(a);
         }
-        vector<int>topo;
-        vector<int>indegree(n,0);
         queue<int>q;
-        for(auto i:m){
-            for(auto j:i.second){
-                indegree[j]++;
-            }
-        }
-        for(int i=0;i<n;i++){
-            if(indegree[i] == 0){
-                q.push(i);
-            }
-        }
+        for(int i=0;i<n;i++)
+            if(indegree[i] == 0) q.push(i);
+        
+        vector<int>v;
         while(!q.empty()){
-            auto node = q.front();
-            topo.push_back(node);
+            int node = q.front();
             q.pop();
+            v.push_back(node);
             for(auto i:m[node]){
                 indegree[i]--;
-                if(indegree[i] == 0){
-                    q.push(i);
-                }
+                if(indegree[i] == 0) q.push(i);
             }
         }
-        if(topo.size() != n) return false;
-        else return true;
+        return (v.size() == n);
     }
 };
